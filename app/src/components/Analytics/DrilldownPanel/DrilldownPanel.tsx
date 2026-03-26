2d87973384e3287002d676a8d123edc3e939a6a4
Parth <parth@vonlabs.ai>
2026-03-27 00:41:26 +0530

    fix: build issue

--- app/src/components/Analytics/DrilldownPanel/DrilldownPanel.tsx
+++ app/src/components/Analytics/DrilldownPanel/DrilldownPanel.tsx
@@ -141,9 +141,7 @@ export const DrilldownPanel: React.FC<DrilldownPanelProps> = ({
           ...(col.sorting ?? {}),
           order:
             col.id === sortState.orderBy
-              ? sortState.orderByAsc
-                ? "asc"
-                : "desc"
+              ? (sortState.orderByAsc ? "asc" : "desc") as const
               : undefined,
         },
       })),
